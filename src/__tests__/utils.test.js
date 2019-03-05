887b26e671ad61ae210c8684247a0f80f728fa56
Yuci Gou <yuci.gou@gmail.com>
2019-03-05 17:36:08 +0000

    Add unit test cases

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const utils = require('../utils')
+
+describe('Utils', () => {
+  it('should return a file with dot UUID as its suffix from the /tmp directory', () => {
+    const filename = 'my-file.pdf'
+    const filepath = utils.getTempPath(filename)
+    const regex = new RegExp(`^/tmp/${filename}.[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`, 'i')
+    expect(filepath).toMatch(regex)
+  })
+
+  it('should remove a given file in the /tmp directory', async () => {
+    const filepath = '/tmp/my-express-middleware-minio-utils-test-file.txt'
+
+    if (fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath)
+    }
+    fs.writeFileSync(filepath, 'Test data')
+    if (!fs.existsSync(filepath)) {
+      throw new Error('Test failed.')
+    }
+
+    utils.removeFile(filepath)
+
+    setTimeout(() => {
+      const exists = fs.existsSync(filepath)
+      expect(exists).toBe(false)
+    }, 1000)
+  })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,19 @@
 const fs = require('fs')
 const uuidv1 = require('uuid/v1')
+const config = require('config')
+
+const logger = (config && config.logger) || console
 
 const tempDir = '/tmp'
 const getTempPath = filename => `${tempDir}/${filename}.${uuidv1()}`
 
 const removeFile = tmpFile => {
+  const regex = new RegExp(`^${tempDir}/`)
+  if (!tmpFile || !tmpFile.match(regex)) {
+    logger.error('Invalid file: ', tmpFile)
+    return
+  }
+
   fs.unlink(tmpFile, err => {
     if (err) {
       console.warn(`Error deleting file ${tmpFile}: `, err)
