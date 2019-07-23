def06c0154b46684c941ace056bb358f57569282
Yuci Gou <yuci.gou@gmail.com>
2019-07-24 00:12:13 +0100

    Add test report

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ module.exports = {
     '<rootDir>/config/',
     '<rootDir>/src/__tests__/test-helper.js'
   ],
-  collectCoverageFrom: ['!src/__tests__/test-helper.js', 'src/**/*.js']
+  collectCoverageFrom: ['!src/__tests__/test-helper.js', 'src/**/*.js'],
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: 'Test Report'
+      }
+    ]
+  ]
 }
