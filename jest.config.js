b9bb753495f596797a92b026d838b7c952e8d5eb
Yuci Gou <yuci.gou@gmail.com>
2019-07-23 16:47:39 +0100

    Let the test script use jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   modulePathIgnorePatterns: [
     '<rootDir>/config/',
     '<rootDir>/src/__tests__/test-helper.js'
-  ]
+  ],
+  collectCoverageFrom: ['!src/__tests__/test-helper.js', 'src/**/*.js']
 }
